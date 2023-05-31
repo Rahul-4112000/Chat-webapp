6f931253b635cb70201b8efe88be0ca79126f71f
Sanjay <sanjay@digitaltrons.com>
2023-05-31 22:33:27 +0530

    feat/chat with feat/codeMessage

--- src/Home/Chat/Chat.js
+++ src/Home/Chat/Chat.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './Chat.css';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import * as themes from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-function Chat({conversations}) {
-
-  return (
-
-    conversations.map((text) => {
-        return(
-            <div></div>
-        )
-    })
-  )
+function Chat({ conversations, userId }) {
+  return (conversations || []).map((item) => {
+    return (
+      <>
+        <div className={userId === item.from ? 'self' : 'from'}>
+          {item.code ? (
+            <p>
+              <SyntaxHighlighter
+                language={item.language}
+                style={themes[item.theme]}
+              >
+                {item.message}
+              </SyntaxHighlighter>
+            </p>
+          ) : (
+            <p>{item.message}</p>
+          )}
+          <p>{item.timestamp.time}</p>
+        </div>
+      </>
+    );
+  });
 }
 
-export default Chat
+export default Chat;
--- src/Home/Chat/Rightcolumn.js
+++ src/Home/Chat/Rightcolumn.js
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import './Rightcolumn.css';
 import Chat from './Chat';
+import * as themes from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import supportedLanguages from 'react-syntax-highlighter/dist/cjs/languages/hljs/supported-languages';
 
 function Rightcolumn({ loginUserData, chattingUserData }) {
-
   const getChatfromLS = () => {
     if (localStorage.getItem('chats')) {
       return JSON.parse(localStorage.getItem('chats'));
-    }
-    else return []
-  }
+    } else return [];
+  };
 
-  
+  const getFilteredChats = () => {
+    const chatdata = getChatfromLS();
+    const filterdChats = chatdata.filter(
+      (chat) =>
+        (chat.from === loggedUserId || chat.from === userId) &&
+        (chat.to === loggedUserId || chat.to === userId)
+    );
+    setFilteredChats(filterdChats);
+  };
+
   const [chat, setChat] = useState('');
   const { avtar, userName, status, userId } = chattingUserData;
   const { userId: loggedUserId } = loginUserData;
   const [chats, setChats] = useState(getChatfromLS());
   const [filteredChats, setFilteredChats] = useState([]);
-  
+  const [isCode, setIsCode] = useState(false);
+  const defaultLanguage = 'javascript';
+  const defaultTheme = 'agate';
+  const [language, setLanguage] = useState(defaultLanguage);
+  const [theme, setTheme] = useState(defaultTheme);
+
   const chatHandler = (e) => {
     setChat(e.target.value);
-  }
-  
-  const getFilteredChats = () => {
-    const chatdata = getChatfromLS();
-    const filterdChats = chatdata.map((chat => {
-      if ((chat.from === loggedUserId || chat.from === userId) && (chat.to === loggedUserId || chat.to === userId)) {
-        return chat;
-      }
-    }));
-    console.log(filterdChats, 'filteredChats');
-    
-  }
+  };
 
   useEffect(() => {
     getFilteredChats();
-  },[])
+  }, [userName]);
 
   const createChatData = (e) => {
     e.preventDefault();
@@ -43,48 +46,100 @@ function Rightcolumn({ loginUserData, chattingUserData }) {
     const chatDetail = {
       message: chat,
       from: loggedUserId,
+      code:isCode,
+      theme:theme,
+      language:language,
       to: userId,
       timestamp: {
-        time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" }),
-        date: new Date().toLocaleDateString()
-      }
-    }
-
-    const text = [...chats, chatDetail]
+        time: new Date().toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+        }),
+        date: new Date().toLocaleDateString(),
+      },
+    };
+    const chats = getChatfromLS();
+    const text = [...chats, chatDetail];
 
     setChats(text);
 
     localStorage.setItem('chats', JSON.stringify(text));
 
+    getFilteredChats();
+
     setChat('');
+  };
 
-  }
+  const handleCode = (e) => {
+    if (e.currentTarget.checked) {
+      setIsCode(true);
+    } else {
+      setIsCode(false);
+    }
+  };
 
-  return (
-    chattingUserData ?
-      <div className='right-column-container'>
-        <div className='right-column-wrapper'>
-          <div className='profile-content'>
-            <div className='profile-img'>
-              <img src={avtar} alt='friend' ></img>
-            </div>
-            <div>
-              <div>{userName}</div>
-              <div>{status}</div>
-            </div>
+  return chattingUserData ? (
+    <div className='right-column-container'>
+      <div className='right-column-wrapper'>
+        <div className='profile-content'>
+          <div className='profile-img'>
+            <img src={avtar} alt='friend'></img>
           </div>
-
-          <Chat conversations={filteredChats} />
-
-          <div className='message-bar'>
-            <form onSubmit={createChatData} >
-              <input value={chat} onChange={chatHandler} placeholder='Type Message' ></input>
-            </form>
+          <div>
+            <div>{userName}</div>
+            <div>{status}</div>
           </div>
         </div>
+        <div className='chat-div'>
+          <Chat conversations={filteredChats} userId={loggedUserId} />
+        </div>
+
+        <div className='message-bar'>
+          <form onSubmit={createChatData}>
+            <label>Write Code</label>
+            <input type='checkbox' onChange={handleCode} />
+            {isCode ? (
+              <div>
+                <select
+                  defaultValue={defaultLanguage}
+                  name='languages'
+                  onChange={(e) => setLanguage(e.target.value)}
+                >
+                  {supportedLanguages.map((language, i) => (
+                    <option key={i}>{language}</option>
+                  ))}
+                </select>
+                <select
+                  defaultValue={defaultTheme}
+                  name='themes'
+                  onChange={(e) => setTheme(e.target.value)}
+                >
+                  {Object.keys(themes).map((theme, i) => (
+                    <option key={i}>{theme}</option>
+                  ))}
+                </select>
+                <textarea
+                  value={chat}
+                  onChange={chatHandler}
+                  placeholder='Write some code'
+                />
+              </div>
+            ) : (
+              <input
+                type='text'
+                value={chat}
+                onChange={chatHandler}
+                placeholder='Type Message'
+              ></input>
+            )}
+            <button onClick={createChatData}>Submit</button>
+          </form>
+        </div>
       </div>
-      : <h1>No chat has been done</h1>
-  )
+    </div>
+  ) : (
+    <h1>No chat has been done</h1>
+  );
 }
 
 export default Rightcolumn;
